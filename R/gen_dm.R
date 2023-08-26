efd95c743afcb70e451a0d0bda3e6d4188bdde30
jche <jonche10@gmail.com>
2023-08-25 21:06:38 -0400

    add gen_dm()

---
+++ R/gen_dm.R
@@ -0,0 +1,48 @@
+
+#' Generate (#tx) by (#co) distance matrix
+#'
+#' @param df dataframe
+#' @param scaling vector of scaling constants for covariates
+#' @param metric distance metric to use
+#'
+#' @return (#tx) by (#co) distance matrix
+gen_dm <- function(df,
+                   scaling = 1,
+                   metric = c("maximum", "euclidean", "manhattan")) {
+  metric <- match.arg(metric)
+
+  # pull out df with only covariates
+  covs <- df %>% select(starts_with("X"))
+
+  # row numbers of each tx/co unit
+  tx_obs <- which(as.logical(df$Z))
+  co_obs <- which(as.logical(!df$Z))
+
+  # if scaling is scalar c, scale all covariates by c
+  if (length(scaling) == 1) {
+    scaling <- rep(scaling, ncol(covs))
+  }
+
+  # scale covariate df, according to implicit distance metric
+  #  - note: coerce non-numeric factor columns to integer values
+  #  - note: coerces T/F into 1/2 instead of 1/0, but this is ok
+  # covs becomes:  covs * scaling
+  #               (nxp)    (pxp)
+  #  --> so dist2 is basically using V(x1-x2) rather than (x1-x2),
+  #      for V = scaling
+  #   - this means that euclidean distance has a (V^T V) scale!
+  covs <- covs %>%
+    mutate(across(!where(is.numeric), ~as.numeric(as.factor(.)))) %>%
+    as.matrix() %*%
+    diag(scaling)
+
+  # compute (#tx) x (#co) distance matrix
+  # TODO: any way to make this faster?
+  dm <- flexclust::dist2(matrix(covs[tx_obs,], ncol = ncol(covs)),   # in case only one tx unit
+                         covs[co_obs,],
+                         method = metric)
+  rownames(dm) <- tx_obs
+  colnames(dm) <- co_obs
+
+  return(dm)
+}
